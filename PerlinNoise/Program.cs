ee2f01a22e58ee0bd8d1d841555459b60b3f7055
Dorozhko Anton <dorozhko.a@gmail.com>
2014-04-19 17:30:19 +0700

    bitmap with using

--- PerlinNoise/Program.cs
+++ PerlinNoise/Program.cs
@@ -22,43 +22,47 @@ namespace PerlinNoise
             return result;
         }
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var random = new Random();
             var size = 256;
-            var bitMap = new Bitmap(size, size);
-
-            int baseFrequency = 3;
-            double[] persistance = { 0.5, 0.35, 0.15};
-
-            var gridList = new List<Tuple<GridArgbWrapper, double>>();
-
-            for (int i = 1; i <= 3; i++)
+            using (var bitMap = new Bitmap(size, size))
             {
-                var freq = (int) Math.Pow(baseFrequency, i);
-                var grid = new GridArgbWrapper(size, freq)//,  new SpiralValueGiver(random.Next(120, 255), random.Next(0, 120)))
-                    { TurnColor = Color.White};
 
-                gridList.Add(new Tuple<GridArgbWrapper, double>(grid, persistance[i - 1]));
-            }
+                int baseFrequency = 3;
+                double[] persistance = {0.5, 0.35, 0.15};
 
-            for (int i = 0; i < bitMap.Size.Width; i++)
-            {
-                for (int j = 0; j < bitMap.Size.Height; j++)
+                var gridList = new List<Tuple<GridArgbWrapper, double>>();
+
+                for (int i = 1; i <= 3; i++)
                 {
-                    
-                    bitMap.SetPixel(i, j , Color.FromArgb(
-                                             NormalizeColorValue(gridList.Sum(tuple => tuple.Item1.GetAlpha(i, j) * tuple.Item2)),
-                                             NormalizeColorValue(gridList.Sum(tuple => tuple.Item1.GetRed(i, j) * tuple.Item2)),
-                                             NormalizeColorValue(gridList.Sum(tuple => tuple.Item1.GetGreen(i, j) * tuple.Item2)),
-                                             NormalizeColorValue(gridList.Sum(tuple => tuple.Item1.GetBlue(i, j) * tuple.Item2))
-                                           )
-                                           );
-                    
+                    var freq = (int) Math.Pow(baseFrequency, i);
+                    var grid = new GridArgbWrapper(size, freq)
+                        //,  new SpiralValueGiver(random.Next(120, 255), random.Next(0, 120)))
+                        {TurnColor = Color.White};
+
+                    gridList.Add(new Tuple<GridArgbWrapper, double>(grid, persistance[i - 1]));
                 }
+
+                for (int i = 0; i < bitMap.Size.Width; i++)
+                {
+                    for (int j = 0; j < bitMap.Size.Height; j++)
+                    {
+
+                        bitMap.SetPixel(i, j, Color.FromArgb(
+                            NormalizeColorValue(gridList.Sum(tuple => tuple.Item1.GetAlpha(i, j)*tuple.Item2)),
+                            NormalizeColorValue(gridList.Sum(tuple => tuple.Item1.GetRed(i, j)*tuple.Item2)),
+                            NormalizeColorValue(gridList.Sum(tuple => tuple.Item1.GetGreen(i, j)*tuple.Item2)),
+                            NormalizeColorValue(gridList.Sum(tuple => tuple.Item1.GetBlue(i, j)*tuple.Item2))
+                                                  )
+                            );
+
+                    }
+                }
+
+                bitMap.Save("perlinWithTurnColor3.bmp");
             }
 
-            bitMap.Save("perlinWithTurnColor3.bmp");
         }
     }
 
